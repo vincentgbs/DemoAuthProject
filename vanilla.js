4fb163a75baaa452eca76778a2476ef459490983
Vincent <vincent.gbs11@gmail.com>
2022-08-23 20:59:54 -0400

    Extracting create_editor function

--- vanilla.js
+++ vanilla.js
@@ -1,5 +1,5 @@
-function editor(root) {
-    document.querySelector(root).innerHTML = `
+function editor(parent, type) {
+    document.querySelector(parent).innerHTML = `
     <div class="left_column">
         <h3>Enter your JavaScript below:</h3>
         <div class="code_editor" contenteditable>alert("Hello World");</div>
@@ -10,15 +10,22 @@ function editor(root) {
         <iframe class="runtime_environment"></iframe>
     </div>`;
 
-    document.querySelector(root + ' .run_button').addEventListener("click", function(){
-        var code_editor = document.querySelector(root + ' .code_editor')
+    document.querySelector(parent + ' .run_button').addEventListener("click", function(){
+        var code_editor = document.querySelector(parent + ' .code_editor')
 
-        var runtime_environment = document.querySelector(root + ' .runtime_environment')
-        var code = "<script>" + code_editor.textContent + "</script>"
+        var runtime_environment = document.querySelector(parent + ' .runtime_environment')
+        var code = "<"+type+">" + code_editor.textContent + "</"+type+">"
         runtime_environment.src = "data:text/html;charset=utf-8," + encodeURI(code)
     });
 }
 
+function create_editor(divId, parent, type='script') {
+    child = document.createElement('div');
+    child.setAttribute("id", divId);
+    document.querySelector(parent).appendChild(child)
+    editor('#'+divId, type);
+}
+
 document.addEventListener('DOMContentLoaded', function(event) {
-    editor('#root');
+    create_editor('first_row', '#root');
 });
