c514abac054104e128f8da2e137a11332773b0df
Vincent <vincent.gbs11@gmail.com>
2022-09-25 12:14:31 -0400

    Fleshing out instructions for project

--- backend/sample.js
+++ backend/sample.js
@@ -6,6 +6,10 @@ var db_connection = project.connect('admin', 'password', 'localhost')
 var user = project.select(db_connection, username)
 
 hash = project.hash(password+user['salt'])
+for (let i = 0; i < 10; i++) {
+    hash = project.hash(hash + user['salt'])
+}
+
 if (hash == user['password']) {
     return true;
 } else {
